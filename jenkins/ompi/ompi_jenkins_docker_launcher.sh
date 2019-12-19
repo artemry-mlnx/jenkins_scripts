b5486c5786dc90c5a16f180a286346a4091361be
Artem Ryabov <artemry@mellanox.com>
2019-12-19 19:09:31 +0300

    Debug

--- jenkins/ompi/ompi_jenkins_docker_launcher.sh
+++ jenkins/ompi/ompi_jenkins_docker_launcher.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -eEx
+#!/bin/bash -eE
+
+if [ "$DEBUG" = "true" ]; then
+    set -x
+fi
 
 # WORKSPACE_JENKINS_SCRIPTS - root folder for jenkins_scripts repo
 if [ -z "${WORKSPACE_JENKINS_SCRIPTS}" ]
@@ -21,32 +25,14 @@ then
     exit 1
 fi
 
-OMPI_CI_OS_NAME="centos"
-OMPI_CI_OS_VERSION="7"
-OMPI_CI_OFED="mofed-4.7-1.0.0.1"
-OMPI_CI_IMAGE_NAME="${OMPI_CI_OS_NAME}_ompi:${BUILD_BUILDID}"
+if [ -z "${OMPI_CI_DOCKER_IMAGE_NAME}" ]
+then
+    echo "ERROR: OMPI_CI_DOCKER_IMAGE_NAME is not defined"
+    exit 1
+fi
 
 # Check that you are inside a docker container
 cat /proc/1/cgroup
-
-DOCKER_BUILD_CONTEXT_DIR=/tmp/ompi_build_docker_image_${BUILD_BUILDID}
-rm -rf ${DOCKER_BUILD_CONTEXT_DIR}
-mkdir -p ${DOCKER_BUILD_CONTEXT_DIR}
-
-# Build Docker image
-docker build \
-    --no-cache \
-    --network=host \
-    --rm \
-    --force-rm \
-    --label=ompi \
-    --build-arg OMPI_CI_OS=${OMPI_CI_OS_NAME}:${OMPI_CI_OS_VERSION} \
-    --build-arg OMPI_CI_OFED=${OMPI_CI_OFED} \
-    -f ${WORKSPACE_JENKINS_SCRIPTS}/jenkins/ompi/Dockerfile \
-    -t ${OMPI_CI_IMAGE_NAME} \
-    ${DOCKER_BUILD_CONTEXT_DIR}
-
-rm -rf ${DOCKER_BUILD_CONTEXT_DIR}
 
 docker images
 docker ps -a
@@ -65,10 +51,7 @@ docker run \
     --security-opt seccomp=unconfined \
     --cap-add=SYS_ADMIN \
     --device=/dev/infiniband/ \
-    --env WORKSPACE=${WORKSPACE_OMPI} \
-    ${OMPI_CI_IMAGE_NAME} \
-    ${WORKSPACE_JENKINS_SCRIPTS}/jenkins/ompi/ompi_jenkins.sh
-
-docker rmi ${OMPI_CI_IMAGE_NAME}
-docker images
-docker ps -a
+    --env WORKSPACE="${WORKSPACE_OMPI}" \
+    --env DEBUG="${DEBUG}" \
+    "${OMPI_CI_DOCKER_IMAGE_NAME}" \
+    "${WORKSPACE_JENKINS_SCRIPTS}/jenkins/ompi/ompi_jenkins.sh"
